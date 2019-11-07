4c156f8f787bca502ca5c6a20df802bf0827a22a
duoduo666 <52728221+duoduo666@users.noreply.github.com>
2019-11-07 18:54:51 +0800

    Update and rename simple-shape-size.txt to simple-shape-size.py

---
+++ shape-size/simple-shape-size.py
@@ -0,0 +1,12 @@
+#quad
+def squa(a,b):
+    return a * b
+#triangle
+def stri (side,high):
+    return side * high / 2
+# trapezoid
+def stra(up,down,high):
+    return (up + down) * high / 2
+#circle
+def scir(half):
+    return half * 2 * 3.141592653
