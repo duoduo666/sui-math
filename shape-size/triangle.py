d855aec53b6321ffe5298604e6dfcfbfeadcd37a
duoduo666 <52728221+duoduo666@users.noreply.github.com>
2019-11-07 18:56:46 +0800

    Update triangle.py

--- shape-size/triangle.py
+++ shape-size/triangle.py
@@ -1,8 +1,12 @@
 pi = 3.1415926535898
-dev = lambda fri,sec : fri / sec
-plus = lambda fri,sec : fri + sec
-min = lambda fri,sec : fri - sec
-mut = lambda fri,sec : fri * sec
+def dev(fri,sec):
+    return fri / sec
+def plus(fri,sec):
+    return fri + sec
+def min(fri,sec):
+    return fri - sec
+def mut(fri,sec):
+    return fri * sec
 def fuc(num):
     if num == 1:
         return num
@@ -19,7 +23,7 @@ def expo(num,time):
         for i in range(1,time):
             num *= y
         return num
-
+​
 #三角函数 sin   
 def sin(angle):
     angle = angle % (pi * 2)
@@ -34,8 +38,7 @@ def sin(angle):
     plus = list1 + list3 + list5 + list7
     minus = list2 + list4 + list6 + list8
     return plus - minus
-    
-
+  
 def cos(angle):
     angle = angle % (pi * 2)
     list1 = dev(expo(angle,2),fuc(2))
@@ -49,21 +52,25 @@ def cos(angle):
     plus = list1 + list3 + list5 + list7
     minus = list2 + list4 + list6 + list8
     return 1 - plus + minus
-
+    
 def tan(angle):
     return sin(angle) / cos(angle)
-
+   
 #反三角函数
 def asin(angle):
     list1 = dev(expo(angle,3),6)
     list2 = mut(dev(3,40),expo(angle,5))
     list3 = mut(dev(5,221),expo(angle,7))
     list4 = mut(dev(35,1152),expo(angle,9))
-    if angle >= 1:
+    if angle > 1:
+        return "are you kidding me. the number you type is gigger than 1"
+    elif angle <= -1:
+        return "are you kidding me. the number you type is smaller than 1"
+    elif angle == 1:
         return 0
-    else:
+    if angle < 1 and angle > -1:
         return angle + list1 + list2 + list3 + list4
-
+​
 def atan(angle):
     list1 = dev(expo(angle,3),3)
     list2 = dev(expo(angle,5),5)
@@ -78,7 +85,6 @@ def atan(angle):
         return 0
     else:
         return angle - minus + plus
-
 def acos(angle):
     list1 = 1 / 2 * pi
     list2 = dev(expo(angle,3),6)
@@ -89,17 +95,21 @@ def acos(angle):
         return 0
     else:
         return list1 - angle - list2 - list3 - list4 - list5
-
+​
 #正割(1/cos)
 def sec(angle):
-    angle = angle % pi
     list1 = dev(expo(angle,2),2)
     list2 = mut(dev(5,24),expo(angle,4))
     list3 = mut(dev(61,720),expo(angle,6))
-    return 1 + list1 + list2 + list3
-    print("this answer is wrong. if you find this sentence, please call (China) +10086 +021 13601946763. it is a free phone number.")
+    awn = 1 + list1 + list2 + list3
+    if awn > 3:
+        word = "it might be wrong，because the number you type in sec is too large "
+        print(word)
+        return awn
+    elif awn <= 3:
+        return awn
     #有错误
-
+​
 #余割(1/sin)   
 def csc(angle):
     angle = angle % pi
@@ -110,12 +120,13 @@ def csc(angle):
     list5 = mut(dev(73,3421440),expo(angle,9))
     list6 = mut(dev(1414477,65383718400),expo(angle,11))
     return (1 / angle) + list1 + list2 + list3 + list4 + list5 + list6
-
+​
 #就是那个tan的倒数
 def cot(angle):
     return cos(angle) / sin(angle)
-
+​
 #双面函数
+​
 #sinh
 def sinh(angle):
     list1 = dev(expo(angle,3),fuc(3))
@@ -129,7 +140,7 @@ def sinh(angle):
     list9 = dev(expo(angle,19),fuc(19))
     list10 = dev(expo(angle,21),fuc(21))
     return angle + list1 + list2 + list3 + list4 + list5 + list6 + list7 + list8 + list9 + list10
-
+​
 #cosh
 def cosh(angle):
     list1 = dev(expo(angle,2),fuc(2))
@@ -142,12 +153,50 @@ def cosh(angle):
     list8 = dev(expo(angle,16),fuc(16))
     list9 = dev(expo(angle,18),fuc(18))
     return 1 + list1 + list2 + list3 + list4 + list5 + list6 + list7 + list8 + list9
-
+   
 #tanh
 def tanh(angle):
     list1 = dev(expo(angle,3),3)
     list2 = mut(dev(2,15),expo(angle,5))
     list3 = mut(dev(17,315),expo(angle,7))
     list4 = mut(dev(62,2835),expo(angle,9))
-    list5 = mut(dev(1382,155925),expo(angle,11))
+    list5 = mut(dev(1328,155925),expo(angle,11))
     return angle - list1 + list2 - list3 + list4 - list5
+​
+#反曲面函数
+#asinh
+def asinh(angle):
+    if angle > 1:
+        return "are you kidding me, the number you type in asinh is bigger than 1."
+    elif angle < -1:
+        return "are you kidding me, the number you type in asinh is smaller than -1"
+    elif angle == 1:
+        return 0
+    elif angle < 1 and angle > -1:
+        list1 = dev(expo(angle,3),6)
+        list2 = mut(dev(3,40),expo(angle,5))
+        list3 = mut(dev(5,112),expo(angle,7))
+        list4 = mut(dev(35,1152),expo(angle,9))
+        return angle - list1 + list2 - list3 + list4
+#太搞了
+def acosh(angle):
+    pass
+    
+def atanh(angle):
+    if angle > 1:
+        return "are you kidding me, the number you type in atanh is bigger than 1." 
+    elif angle < -1:
+        return "are you kidding me, the number you type in atanh is smaller than -1"
+    elif angle == 1:
+        return 0
+    elif angle < 1 and angle > -1:
+        list1 = dev(expo(angle,3),3)
+        list2 = dev(expo(angle,5),5)
+        list3 = dev(expo(angle,7),7) 
+        list4 = dev(expo(angle,9),9)
+        list5 = dev(expo(angle,11),11)
+        list6 = dev(expo(angle,13),13)
+        list7 = dev(expo(angle,15),15)
+        list8 = dev(expo(angle,17),17)
+        list9 = dev(expo(angle,19),19)
+        return angle + list1 + list2 + list3 + list4 + list5 + list6 + list7 + list8 + list9
